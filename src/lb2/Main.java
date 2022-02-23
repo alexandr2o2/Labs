89cd644191266a9a01b4f199f74d774e7a7788ca
alexandr2o2 <aaz75@tpu.ru>
2022-02-23 11:00:29 +0700

    Основная структура

--- src/lb2/Main.java
+++ src/lb2/Main.java
@@ -3,6 +3,32 @@ package lb2;
 public class Main {
     static public void main(String[] args){
 
+        Thread consumer = new Thread(new Consumer());
+        consumer.start();
+        Thread manufacturer = new Thread(new Manufacturer());
+        manufacturer.start();
 
+    }
+}
+
+class Manufacturer implements Runnable{
+
+    private int output = 10;
+
+    @Override
+    public void run(){
+        System.out.println("Производитель");
+        System.out.println("Имя потока: "+Thread.currentThread().getName());
+    }
+}
+
+class Consumer implements Runnable{
+
+    private int input = 5;
+
+    @Override
+    public void run(){
+        System.out.println("Потребитель ");
+        System.out.println("Имя потока: "+Thread.currentThread().getName());
     }
 }
---
+++ src/lb2/Storage.java
@@ -0,0 +1,14 @@
+package lb2;
+
+public class Storage {
+    public int getNumOfProduct() {
+        return numOfProduct;
+    }
+
+    public void setNumOfProduct(int numOfProduct) {
+        this.numOfProduct = numOfProduct;
+    }
+
+    private int numOfProduct;
+
+}
