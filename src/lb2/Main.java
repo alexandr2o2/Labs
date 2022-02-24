a2882e629527cbe0c3f0ce385a94a191e1bbc171
alexandr2o2 <aaz75@tpu.ru>
2022-02-24 22:10:24 +0700

    Вроде работает \(0_0)/?

--- src/lb2/Main.java
+++ src/lb2/Main.java
@@ -3,32 +3,73 @@ package lb2;
 public class Main {
     static public void main(String[] args){
 
-        Thread consumer = new Thread(new Consumer());
-        consumer.start();
-        Thread manufacturer = new Thread(new Manufacturer());
-        manufacturer.start();
+        Storage storage = new Storage(250);
+
+        Consumer consumer = new Consumer();
+        consumer.addStorage(storage);
+
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.addStorage(storage);
+
+        Thread manufacturerThread = new Thread(manufacturer);
+        manufacturerThread.start();
+
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
 
     }
 }
 
 class Manufacturer implements Runnable{
 
-    private int output = 10;
+    private int output = 5;
+    Storage storage;
+
+    public void addStorage(Storage storage){
+        this.storage = storage;
+    }
 
     @Override
     public void run(){
-        System.out.println("Производитель");
-        System.out.println("Имя потока: "+Thread.currentThread().getName());
+        while (true){
+            if (storage.addNumOfProduct(output)) {
+                System.out.println("Произведен товар. Количество товара на складе " + storage.getNumOfProduct());
+            }
+            else {
+                System.out.println("Произведен товар. Cклад полн!!");
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
 class Consumer implements Runnable{
 
-    private int input = 5;
+    private int input = 10;
+    Storage storage;
+
+    public void addStorage(Storage storage){
+        this.storage = storage;
+    }
 
     @Override
     public void run(){
-        System.out.println("Потребитель ");
-        System.out.println("Имя потока: "+Thread.currentThread().getName());
+        while (true){
+            if(storage.removeNumOfProduct(input)) {
+                System.out.println("Потреблен товар. Количество товара на складе " + storage.getNumOfProduct());
+            }
+            else {
+                System.out.println("Cклад пуст!");
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/lb2/Storage.java
+++ src/lb2/Storage.java
@@ -1,14 +1,35 @@
 package lb2;
 
 public class Storage {
+
+    Storage(int numOfProduct){
+        this.numOfProduct = numOfProduct;
+    }
     public int getNumOfProduct() {
         return numOfProduct;
     }
 
-    public void setNumOfProduct(int numOfProduct) {
+    public synchronized void setNumOfProduct(int numOfProduct) {
         this.numOfProduct = numOfProduct;
     }
 
-    private int numOfProduct;
+    private volatile int numOfProduct;
+    private int capacity = 1000;
 
+    public synchronized boolean addNumOfProduct(int numOfProduct){
+        if (this.numOfProduct < capacity - numOfProduct){
+            this.numOfProduct = this.numOfProduct + numOfProduct;
+            return true;
+        }
+        else
+            return false;
+    }
+    public synchronized boolean removeNumOfProduct(int numOfProduct){
+        if (this.numOfProduct - numOfProduct > 0){
+            this.numOfProduct = this.numOfProduct - numOfProduct;
+            return true;
+        }
+        else
+            return false;
+    }
 }
